0671f41b22be7b026d62d1900c70f7c79ebcc3f3
ZdravkoHr <zdravkohristov0@gmail.com>
2023-03-15 11:23:57 +0200

    add ui for initiating a CV

--- app/(components)/CV.js
+++ app/(components)/CV.js
@@ -1,18 +1,14 @@
 "use client";
 import { useContext } from "react";
 import { CVContext } from "../ContextProvivder";
+import CreateCV from "./CreateCV";
 import styles from "./CV.module.scss";
 
 export default function CV() {
   const { data } = useContext(CVContext);
 
   if (!data) {
-    return (
-      <div className={styles.no_cv_buttons}>
-        <button>New +</button>
-        <button>Import</button>
-      </div>
-    );
+    return <CreateCV />;
   }
 
   return <p>asdadadss</p>;
---
+++ app/(components)/CreateCV.js
@@ -0,0 +1,31 @@
+import { useState, useContext } from "react";
+import { CVContext } from "../ContextProvivder";
+import styles from "./CV.module.scss";
+
+export default function CreateCV() {
+  const [number, setNumber] = useState(1);
+  const { data, setData } = useContext(CVContext);
+
+  function submitHandler() {
+    setData({ ...data, elderNumber: number });
+  }
+
+  return (
+    <div>
+      <div className={styles.no_cv_buttons}>
+        <button>New +</button>
+        <button>Import</button>
+      </div>
+      <form className={styles.create_holder} onSubmit={submitHandler}>
+        <input
+          type="number"
+          min="1"
+          placeholder="Elder №"
+          value={number}
+          onChange={(e) => setNumber(+e.target.value)}
+        />
+        <button>Create</button>
+      </form>
+    </div>
+  );
+}
