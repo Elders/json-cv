348ba832e7b5dabe2ed09eb85c14b65230d931b4
ZdravkoHr <zdravkohristov0@gmail.com>
2023-04-13 13:20:25 +0300

    getCVById without making a request when on the server

--- app/cv/[id]/page.js
+++ app/cv/[id]/page.js
@@ -1,24 +1,11 @@
 import StoreProvider from "@/app/(components)/StoreProvider";
-
 import CVContent from "@/app/(components)/CVContent";
-import Navbar from "./Navbar";
-import store from "@/store/store";
-import { setData as setAppData } from "@/store/slices/app";
 import findCV from "@/helpers/findCV";
+import getCVByID from "@/helpers/getCVById";
 
 export default async function CVPage({ params }) {
-  let cv = findCV(params.id);
-
-  if (!cv) {
-    const response = await fetch(
-      `${process.env.HOST}/api/getCVByID?id=${params.id}`,
-      { cache: "no-store" }
-    );
-
-    cv = await response.json();
-  }
-
-  // store.dispatch(setAppData({ cv }));
+  const storedCV = findCV(params.id);
+  const cv = storedCV || (await getCVByID(params.id));
 
   return (
     <StoreProvider>
---
+++ helpers/getCVById.js
@@ -0,0 +1,6 @@
+import readCVS from "./readCVS";
+
+export default async function getCVByID(id) {
+  const cvs = await readCVS();
+  return cvs.find((cv) => cv.id === id);
+}
