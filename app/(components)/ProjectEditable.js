22fc79fce691d31359acc9c5278a0ff7a6a714d2
ZdravkoHr <zdravkohristov0@gmail.com>
2023-06-06 09:30:08 +0300

    change projectsEditable to projectEditable, change the structure in edit mode to look like in view mode, and improve scroll calculations

---
+++ app/(components)/ProjectEditable.js
@@ -0,0 +1,143 @@
+import { useRef } from "react";
+import cardStyles from "@/app/(styles)/card.module.scss";
+import store from "@/store/store";
+import MultipleInputs from "./MultipleInputs";
+import {
+  editProject,
+  deleteProject,
+  swapOpenSourceProjects,
+} from "@/store/slices/app";
+import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
+import MarkdownElement from "./MarkdownElement";
+
+export default function ProjectEditable({ project, index, length }) {
+  const ref = useRef();
+
+  function editHandler(prop, value, id) {
+    store.dispatch(
+      editProject({
+        id,
+        prop,
+        value,
+      })
+    );
+  }
+
+  function deleteHandler(id) {
+    store.dispatch(deleteProject(id));
+  }
+
+  function moveProject(start, moveBy) {
+    const end = start + moveBy;
+    console.log(ref.current.getBoundingClientRect());
+
+    store.dispatch(swapOpenSourceProjects([start, end]));
+    const { height, top, bottom, y } = ref.current.getBoundingClientRect();
+
+    if (moveBy < 0) {
+      const bottomScroll = Math.max(
+        bottom - document.documentElement.clientHeight,
+        0
+      );
+
+      return window.scrollBy(0, bottomScroll - height);
+    }
+
+    const scrollValue = height + top - 60;
+    window.scrollBy(0, scrollValue * moveBy);
+  }
+
+  return (
+    <div
+      className={`${cardStyles.card} ${cardStyles.project_card} mb-3 flex`}
+      key={project.id}
+      ref={ref}
+    >
+      <main className="grow">
+        {" "}
+        <header>
+          <h4 className="my-1">Project name: </h4>
+          <input
+            className={cardStyles.project_name_input}
+            type="text"
+            value={project.name}
+            required
+            onChange={(e) => editHandler("name", e.target.value, project.id)}
+          />
+          <div>
+            <h4 className="my-1">Role:</h4>
+            <input
+              className={cardStyles.project_name_input}
+              type="text"
+              value={project.role}
+              onChange={(e) => editHandler("role", e.target.value, project.id)}
+            ></input>
+          </div>
+        </header>
+        <div>
+          <MarkdownElement
+            sectionHeading={<h4 className="my-1">Description:</h4>}
+            canEdit={true}
+            markdownContent={project.description}
+          >
+            <textarea
+              value={project.description}
+              onChange={(e) =>
+                editHandler("description", e.target.value, project.id)
+              }
+            ></textarea>{" "}
+            <p className="hints align-right">Supports Markdown</p>
+          </MarkdownElement>
+        </div>
+        <div>
+          <h4 className="my-1">Environment:</h4>
+          <input
+            className={cardStyles.project_name_input}
+            value={project.environment}
+            onChange={(e) =>
+              editHandler("environment", e.target.value, project.id)
+            }
+          />
+        </div>
+        <div>
+          <h4 className="my-1">References:</h4>
+        </div>
+        <MultipleInputs
+          defaultLabels={project.labels}
+          items={project.references || []}
+          showLabel={true}
+          mainLabelText="URL: "
+          className={cardStyles.references}
+          onChange={(references, labels) => {
+            editHandler(
+              "references",
+              references?.filter(Boolean) || [],
+              project.id
+            );
+            editHandler("labels", labels?.filter(Boolean) || [], project.id);
+          }}
+        />
+        <div className="mt-1">
+          <button onClick={() => deleteHandler(project.id)}>Delete</button>
+        </div>
+      </main>{" "}
+      <div className={cardStyles.reorder_parent}>
+        {index ? (
+          <ChevronUpIcon
+            size={35}
+            color="#ccc"
+            onClick={() => moveProject(index, -1)}
+          />
+        ) : null}
+
+        {index !== length - 1 ? (
+          <ChevronDownIcon
+            size={35}
+            color="#ccc"
+            onClick={() => moveProject(index, 1)}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+}
--- app/(components)/ProjectsSection.js
+++ app/(components)/ProjectsSection.js
@@ -1,18 +1,71 @@
-import { useSelector } from 'react-redux';
-import ProjectsEditable from './ProjectsEditable';
-import cardStyles from '@/app/(styles)/card.module.scss';
-import projectIcon from '@/assets/project-icon.svg';
-import Image from 'next/image';
-import Link from './Link';
-import MarkdownElement from './MarkdownElement';
+import { useSelector } from "react-redux";
+import ProjectEditable from "./ProjectEditable";
+import cardStyles from "@/app/(styles)/card.module.scss";
+import projectIcon from "@/assets/project-icon.svg";
+import Image from "next/image";
+import Link from "./Link";
+import MarkdownElement from "./MarkdownElement";
+
+function ProjectCard({ project }) {
+  return (
+    <div key={project.id}>
+      <div className="empty-div"></div>
+      <div
+        className={`${cardStyles.card} ${cardStyles.project_card}`}
+        key={project.id}
+      >
+        <header className={cardStyles.project_header}>
+          <div className={cardStyles.project_name}>
+            <Image
+              src={projectIcon}
+              className={cardStyles.project_icon}
+              alt="project icon"
+            />
+            <h2>{project.name}</h2>
+          </div>
+          {!project.role ? null : (
+            <div>
+              <h4 className="column-name">ROLE:</h4>
+              <h5>{project.role}</h5>
+            </div>
+          )}
+        </header>
+        <main>
+          <MarkdownElement markdownContent={project.description} />
+        </main>
+
+        {project.environment ? (
+          <div>
+            <h4>ENVIRONMENT:</h4>
+            <p>{project.environment}</p>
+          </div>
+        ) : null}
+
+        {project.references?.length ? (
+          <div className={cardStyles.references}>
+            <h4>REFERENCES</h4>
+            <div>
+              {project.references.map((reference, index) => {
+                const placeholder = project.labels
+                  ? project.labels[index]
+                  : reference;
+                return (
+                  <div className="mt-1" key={index}>
+                    <Link link={reference} placeholder={placeholder} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
 
 export default function ProjectsSection({ projects }) {
   const { isEditing, cv: cvData } = useSelector((state) => state.app);
   const appProjects = cvData?.projects || [];
-
-  if (isEditing) {
-    return <ProjectsEditable projects={appProjects} />;
-  }
 
   return (
     <div>
@@ -21,61 +74,15 @@ export default function ProjectsSection({ projects }) {
           <header className={cardStyles.header}>
             <h2>Open-source projects</h2>
           </header>
-          {projects?.map((project) => {
-            return (
-              <div key={project.id}>
-              <div className="empty-div"></div>
-              <div
-                className={`${cardStyles.card} ${cardStyles.project_card}`}
-                key={project.id}
-              >
-                <header className={cardStyles.project_header}>
-                  <div className={cardStyles.project_name}>
-                    <Image
-                      src={projectIcon}
-                      className={cardStyles.project_icon}
-                      alt="project icon"
-                    />
-                    <h2>{project.name}</h2>
-                  </div>
-                  {!project.role ? null : (
-                    <div>
-                      <h4 className="column-name">ROLE:</h4>
-                      <h5>{project.role}</h5>
-                    </div>
-                  )}
-                </header>
-                <main>
-                  <MarkdownElement markdownContent={project.description} />
-                </main>
-
-                {project.environment ? (
-                  <div>
-                    <h4>ENVIRONMENT:</h4>
-                    <p>{project.environment}</p>
-                  </div>
-                ) : null}
-
-                {project.references?.length ? (
-                  <div className={cardStyles.references}>
-                    <h4>REFERENCES</h4>
-                    <div>
-                      {project.references.map((reference, index) => {
-                        const placeholder = project.labels
-                          ? project.labels[index]
-                          : reference;
-                        return (
-                          <div className="mt-1" key={index}>
-                            <Link link={reference} placeholder={placeholder} />
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                ) : null}
-              </div>
-              </div>
-              
+          {projects?.map((project, index) => {
+            return isEditing ? (
+              <ProjectEditable
+                project={project}
+                index={index}
+                length={projects.length}
+              />
+            ) : (
+              <ProjectCard project={project} />
             );
           })}
         </section>
