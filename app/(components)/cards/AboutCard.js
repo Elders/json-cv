5de7a06e37074a158ba7d7bae6c20c57fe2d8152
Stanimir87 <dimov.stanimir@gmail.com>
2023-06-12 13:12:43 +0300

    add about section in CV

---
+++ app/(components)/cards/AboutCard.js
@@ -0,0 +1,29 @@
+import styles from "app/(styles)/card.module.scss";
+import { useSelector } from "react-redux";
+import { updateCv } from "@/store/slices/app";
+import store from "@/store/store";
+
+function AboutCard({ info }) {
+
+  const { isEditing } = useSelector((state) => state.app);
+
+  function editHandler(e) {
+    store.dispatch(updateCv({ info: e.target.value }));
+  }
+
+  if (!info && !isEditing) {
+    return null;
+  }
+  return (
+    <div className={styles.card}>
+      <h2 className="mb-1">About: </h2>
+      {isEditing ? (
+        <textarea value={info || ""} onChange={editHandler}></textarea>
+      ) : (
+        <p>{info}</p>
+      )}
+    </div>
+  );
+}
+
+export default AboutCard;
