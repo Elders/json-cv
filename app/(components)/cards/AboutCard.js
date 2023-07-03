3e462b0a0c8acee2eb1626f6ad65125b6e71298b
ZdravkoHr <zdravkohristov0@gmail.com>
2023-07-03 16:53:22 +0300

    fix cursor jumping in about textarea

--- app/(components)/cards/AboutCard.js
+++ app/(components)/cards/AboutCard.js
@@ -2,23 +2,26 @@ import styles from "app/(styles)/card.module.scss";
 import { useSelector } from "react-redux";
 import { updateCv } from "@/store/slices/app";
 import store from "@/store/store";
+import { useState } from "react";
 
 function AboutCard({ info }) {
-
   const { isEditing } = useSelector((state) => state.app);
+  const [inputValue, setInputValue] = useState();
 
   function editHandler(e) {
+    setInputValue(e.target.value);
     store.dispatch(updateCv({ info: e.target.value }));
   }
 
   if (!info && !isEditing) {
     return null;
   }
+
   return (
     <div className={styles.card}>
       <h2 className="mb-1">About: </h2>
       {isEditing ? (
-        <textarea value={info || ""} onChange={editHandler}></textarea>
+        <textarea value={inputValue} onChange={editHandler}></textarea>
       ) : (
         <p>{info}</p>
       )}
